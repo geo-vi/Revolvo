3b9cb323b8b10c0601f1c0dcddb0388e173de6e2
CallMeHeisenberg <ic3.sh0ck@gmail.com>
2019-04-01 09:56:42 +0300

    -removed xsocket - policy client started

--- Revolvo/Networking/remote_servers/PolicyClient.cs
+++ Revolvo/Networking/remote_servers/PolicyClient.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
 using Revolvo.Bot.managers;
 using Revolvo.Bot.netty;
 using Revolvo.Main;
@@ -14,40 +17,22 @@ namespace Revolvo.Networking.remote_servers
 {
     class PolicyClient
     {
-        public XSocket XSocket { get; }
-
         private PolicyServer Server;
 
         public PolicyClient(PolicyServer server)
         {
-            var ip = StorageManager.GetIP();
-            XSocket = new XSocket(ip, Defaults.DEFAULT_POLICY_PORT);
-            XSocket.OnReceive += XSocket_OnReceive;
-            XSocket.ConnectionClosedEvent += XSocket_ConnectionClosedEvent;
             Server = server;
         }
 
-        public void Connect()
+        public async void Connect()
         {
-            XSocket.Connect();
-            XSocket.Read(true);
-        }
-
-        private void XSocket_ConnectionClosedEvent(object sender, EventArgs e)
-        {
+            //TODO: Connect
+            var ip = StorageManager.GetIP();
+            var bootstrap = new Bootstrap();
+            IChannel bootstrapChannel = await bootstrap.ConnectAsync(ip, Defaults.DEFAULT_POLICY_PORT);
 
         }
 
-        private void XSocket_OnReceive(object sender, EventArgs e)
-        {
-            MainController.Instance.Session.GameServer = new GameServer();
-            var args = (StringArgs)e;
-            var packet = args.Packet;
-            Server.XSocket.Write(packet);
-            Console.WriteLine(packet);
-            Server.XSocket.Close();
-            XSocket.Close();
-            //open the gameserver now.
-        }
+
     }
 }
